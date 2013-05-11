7e94a67ad5b29a0748e9eccb8460bdf1280aa278
Mark Murray <markmurray406@gmail.com>
2013-05-11 14:56:49 +0100

    Add Image Uploading via URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,6 @@ Omrails::Application.configure do
         :bucket => ENV['AWS_BUCKET'],
         :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
         :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
-}
 end
